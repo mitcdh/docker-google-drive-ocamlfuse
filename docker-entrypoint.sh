ce7c5768b8f21422ddbf7bcee71310d635268b7e
Mitchell Hewes <me@mitcdh.com>
2017-06-14 10:25:49 +1000

    Changed to an su implementation to fix fuse context

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,22 @@ DRIVE_PATH=/mnt/gdrive
 PUID=${PUID:-0}
 PGID=${PGID:-0}
 
-if [ -e "~/.gdfuse/default/config" ]; then
+if [ ! $(getent group gdfuser) ]; then
+          echo "creating gdfuser group for gid ${PGID}"
+          groupadd --gid ${PGID} --non-unique gdfuser >/dev/null 2>&1
+fi
+
+if [ ! $(getent passwd gdfuser) ]; then
+          echo "creating gdfuser group for uid ${PUID}"
+          useradd --gid ${PGID} --non-unique --comment "Google Drive Fuser" \
+           --home-dir "/config" --create-home \
+           --uid ${PUID} gdfuser >/dev/null 2>&1
+
+          echo "taking ownership of /config for gdfuser"
+          chown ${PUID}:${PGID} /config
+fi
+
+if [ -e "/config/.gdfuse/default/config" ]; then
         echo "existing google-drive-ocamlfuse config found"
 else
         if [ -z "${CLIENT_ID}" ]; then
@@ -19,11 +34,15 @@ else
             exit 1
         else
                 echo "initilising google-drive-ocamlfuse..."
-                echo "${VERIFICATION_CODE}" | \
-                        google-drive-ocamlfuse -headless -id "${CLIENT_ID}.apps.googleusercontent.com" -secret "${CLIENT_SECRET}"
+                su gdfuser -l -c  "echo \"${VERIFICATION_CODE}\" | \
+                 google-drive-ocamlfuse -headless \
+                 -id \"${CLIENT_ID}.apps.googleusercontent.com\" \
+                 -secret \"${CLIENT_SECRET}\""
         fi
 fi
 
 echo "mounting at ${DRIVE_PATH}"
-google-drive-ocamlfuse "${DRIVE_PATH}" -o uid=${PUID},gid=${PGID},allow_root
+su gdfuser -l -c "google-drive-ocamlfuse \"${DRIVE_PATH}\"\
+ -o uid=${PUID},gid=${PGID}"
+
 tail -f /dev/null & wait
